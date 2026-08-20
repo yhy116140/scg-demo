53f38b8415a964ee2e01373a49ae51cf54802073
yhy116140 <1161404691@qq.com>
2026-08-20 22:18:54 +0800

    test1

--- vuln.go
+++ vuln.go
@@ -2,7 +2,7 @@ package main
 
 import ("database/sql"; "net/http"; "os/exec")
 
-// 演示用漏洞代码：提交到极狐后 SCG 应报 SQL 注入 + 命令注入
+// 演示用漏洞代码：提交到后 SCG 应报 SQL 注入 + 命令注入
 func demo(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	rows, _ := db.Query("SELECT * FROM users WHERE id = '" + id + "'")
