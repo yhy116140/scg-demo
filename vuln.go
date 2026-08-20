a2750c790df4163d0b0717d7582e1e441c9466f4
yhy116140 <1161404691@qq.com>
2026-08-20 15:59:12 +0800

    test

---
+++ vuln.go
@@ -0,0 +1,12 @@
+package main
+
+import ("database/sql"; "net/http"; "os/exec")
+
+// 演示用漏洞代码：提交到极狐后 SCG 应报 SQL 注入 + 命令注入
+func demo(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	rows, _ := db.Query("SELECT * FROM users WHERE id = '" + id + "'")
+	defer rows.Close()
+	out, _ := exec.Command("sh", "-c", "ping "+id).Output()
+	w.Write(out)
+}
